8ca21d8f4e4dd94ed2d1aafb0d242f5d90f0b3c6
RayMN <muhammadraisya170407@gmail.com>
2023-10-27 11:47:54 +0700

    nambah halaman succes

--- src/pages/succes.jsx
+++ src/pages/succes.jsx
@@ -1,10 +1,24 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image,TouchableOpacity } from 'react-native'
 import React from 'react'
 
 const succes = () => {
   return (
-    <View>
-        
+    <View style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignItems: 'center'
+    }}>
+        <Image style={{
+            width:300,
+            height: 300
+        }}
+        source={require('../assets/icons/icon-correct-24-2 1.png')}/>
+        <Text>Pesanan Berhasil</Text>
+        <Text>Pesanan Anda telah diterima dan akan segera diproses. Mohon ditunggu ya</Text>
+        <TouchableOpacity>
+            <Text>Kembali Ke Menu Utama</Text>
+        </TouchableOpacity>
     </View>
   )
 }
